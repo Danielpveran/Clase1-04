b59f800f2cee913a87fed3115580c35333aeaf85
LizethJs <141794710+Danielpveran@users.noreply.github.com>
2025-04-01 19:29:40 -0500

    1.0.1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from'react';
+
+function App()
+{
+
+    const value = 'World'
+    return<div>Hello {value}</div>;
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Se realiza el import del Archivo App que se conforma como un componente
+import App from './App';
+
+ReactDOM.render(<App/>,
+document,getElementById('root'));
+
